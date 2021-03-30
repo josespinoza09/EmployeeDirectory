b2aadde6241723f6c5220686aa409f80b01e5ccc
Jose Espinoza <jose20019004@gmail.com>
2021-03-30 19:07:14 -0400

    function to sort names

--- src/components/employee.js
+++ src/components/employee.js
@@ -8,7 +8,7 @@ function Employee() {
     const [order, setOrder] = useState('ascending')
     async function getEmployee() {
         const results = await axios.get("https://randomuser.me/api/?results=30")
-        
+
         setData(data => results.data.results)
 
     }
@@ -16,3 +16,28 @@ function Employee() {
     useEffect(function () {
         getEmployee()
     }, [])
+
+    function ascendingSort() {
+        console.log(data)
+        let filtereddata = [...data]
+        console.log(filtereddata)
+        const sortedEmployees = filtereddata.sort((a, b) => {
+            if (b.name.first > a.name.first) {
+                console.log()
+                return -1
+            }
+            if (a.name.first > b.name.first) {
+                return 1
+            }
+            return 0
+        });
+        if (order === 'descending') {
+            sortedEmployees.reverse()
+            setOrder("ascending")
+        }
+        else {
+            setOrder("descending")
+        }
+        setData(sortedEmployees)
+
+    }
