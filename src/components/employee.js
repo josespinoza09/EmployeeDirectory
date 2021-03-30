c04304c338970c04dd04002c6b6bc59ca273b8d6
Jose Espinoza <jose20019004@gmail.com>
2021-03-30 19:06:02 -0400

    added the axios call for the api's

--- src/components/employee.js
+++ src/components/employee.js
@@ -6,3 +6,13 @@ function Employee() {
 
     const [data, setData] = useState([])
     const [order, setOrder] = useState('ascending')
+    async function getEmployee() {
+        const results = await axios.get("https://randomuser.me/api/?results=30")
+        
+        setData(data => results.data.results)
+
+    }
+
+    useEffect(function () {
+        getEmployee()
+    }, [])
